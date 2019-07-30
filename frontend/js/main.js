f58f8db5b33c36b32db6319f5a69cdf4c3000767
TheBombTNT <42681446+TheBombTNT@users.noreply.github.com>
2019-07-30 10:59:33 -0400

    testing website

---
+++ frontend/js/main.js
@@ -0,0 +1,89 @@
+$(document).ready(function(){
+    getAllContacts();
+});
+
+function add() {
+    var contact = { name: $("#name").val(),
+                    phone: $("#phone").val(),
+                    address: $("#address").val() };
+    
+    alert(JSON.stringify(contact));
+    
+    $.post("/add.php", JSON.stringify(contact), function(result){
+        alert(result);
+        });
+    
+    $("#name").val("");
+    $("#phone").val("");
+    $("#address").val("");
+}
+
+function getAllContacts() 
+{
+    $.getJSON("/search.php", contactInfo,function(data) 
+    {
+        var retrieveContact = 
+    });
+        
+        
+//        var contact[];
+//        $.each(contactInfo, function(key, val)
+//        {
+//            contact.push("<tr>");
+//            contact.push("<td id='' "+key+" ''> "+val.name+" </td>");
+//            
+//            contact.push(
+//                "<td id='"+key+"' onClick=loadContact();'> "+val.name" </td>");
+//            
+//            
+//            contact.push("<td id='"+key+" ''> "+val.address+" </td>");
+//            contact.push("</tr>");
+//        });
+        
+    }
+}
+              
+/*
+function search()
+{
+    var srch = document.getElementById("searchbox").value;
+    document.getElementById("colorSearchResult").innerHTML = "";
+    
+    var colorList = document.getElementById("colorList");
+    colorList.innerHTML = "";
+    
+    var jsonPayload = '{"search" : "' + srch + '"}';
+    var url = urlBase + '/SearchColors.' + extension;
+    
+    var xhr = new XMLHttpRequest();
+    xhr.open("POST", url, true);
+    xhr.setRequestHeader("Content-type", "application/json; charset=UTF-8");
+    try
+    {
+        xhr.onreadystatechange = function() 
+        {
+            if (this.readyState == 4 && this.status == 200) 
+            {
+                hideOrShow( "colorList", true );
+                
+                document.getElementById("colorSearchResult").innerHTML = "Color(s) has been retrieved";
+                var jsonObject = JSON.parse( xhr.responseText );
+                
+                var i;
+                for( i=0; i<jsonObject.results.length; i++ )
+                {
+                    var opt = document.createElement("option");
+                    opt.text = jsonObject.results[i];
+                    opt.value = "";
+                    colorList.options.add(opt);
+                }
+            }
+        };
+        xhr.send(jsonPayload);
+    }
+    catch(err)
+    {
+        document.getElementById("colorSearchResult").innerHTML = err.message;
+    }
+    
+}*/
